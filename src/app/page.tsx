a48cb8c8de688e925761d1961355ad561123166c
Argens <argens1203@gmail.com>
2023-07-09 15:59:21 +0800

    feat: server side rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-'use client';
-import { useEffect, useState } from 'react';
-import { getBlogs } from '../firestore/blog';
 import { Blog } from '@/entities/blog.entity';
+import { getBlogs } from '../firestore/blog';
 import { BlogElement } from './blog-element';
 
-export default function Bal() {
-    const [blogs, setBlogs] = useState<Blog[]>([]);
-    useEffect(() => {
-        getBlogs().then(setBlogs);
-    }, []);
+export default async function Bal() {
+    const blogs = await getBlogs();
     return (
         <div>
             {blogs.map((blog: Blog, idx) => (
