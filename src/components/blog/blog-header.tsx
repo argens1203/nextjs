920773e071a9c5e3b012457abb8b4e46b30214a9
Argens <argens1203@gmail.com>
2023-07-17 19:41:10 +0800

    temp: send all "blogs" to under "topics"

--- src/components/blog/blog-header.tsx
+++ src/components/blog/blog-header.tsx
@@ -1,9 +1,22 @@
 'use client';
 
-import { useContext } from 'react';
+import { useCallback, useContext, useState } from 'react';
 import { BlogContext } from './blog-context-provider';
+import { migrateFromBlog } from '@/firestore/topic';
 
 export function BlogHeader() {
     const { idsChosen } = useContext(BlogContext);
-    return <p>{idsChosen.join(',')}</p>;
+    const [sending, setSending] = useState(false);
+    const send = useCallback(() => {
+        setSending(true);
+        Promise.all(idsChosen.map((id) => migrateFromBlog(id)))
+            .then(console.log)
+            .finally(() => setSending(false));
+    }, [idsChosen]);
+    return (
+        <div>
+            <button onClick={send}>{sending ? 'Sending' : 'Send'}</button>
+            <p>{idsChosen.join(',')}</p>
+        </div>
+    );
 }
